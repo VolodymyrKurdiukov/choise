a4f3f3a47b8052326f6eb1f3ed19dcc409ee6b78
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-09-09 23:20:42 +0300

    asideUserSlider

--- dist/js/script.js
+++ dist/js/script.js
@@ -75,4 +75,8 @@ $(document).ready(function () {
 	$('.lang-top__link').click(function (event) {
 		$(this).toggleClass('active').next().slideToggle(300);
 	});
+
+	$('.menu-page__link-arrow').click(function (event) {
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
 });
